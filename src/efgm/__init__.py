022c01fbe2b325c35d4c75dc88f669aa55f9cd45
Dan Auffrey <42249260+dauffrey@users.noreply.github.com>
2026-08-07 19:02:08 -0300

    Export research validation helpers

--- src/efgm/__init__.py
+++ src/efgm/__init__.py
@@ -1,8 +1,20 @@
-from .baselines import checklist_baseline, grounding_calibration_baseline, weighted_linear_baseline
+from .baselines import (
+    checklist_baseline,
+    grounding_calibration_baseline,
+    independent_checklist_baseline,
+    weighted_linear_baseline,
+)
 from .schemas import EFGMInput, EFGMResult
 from .schemas_v2 import EFGMDecisionInput, EFGMDecisionResult, MetricObservation
 from .scoring import score_efgm
-from .scoring_v2 import load_scoring_config, score_decision_efgm
+from .scoring_v2 import (
+    IncompleteAssessmentError,
+    ProvenanceError,
+    canonical_sha256,
+    load_scoring_config,
+    research_provenance_issues,
+    score_decision_efgm,
+)
 
 __version__ = "0.2.0"
 
@@ -12,10 +24,15 @@ __all__ = [
     "MetricObservation",
     "EFGMDecisionInput",
     "EFGMDecisionResult",
+    "IncompleteAssessmentError",
+    "ProvenanceError",
+    "canonical_sha256",
     "load_scoring_config",
+    "research_provenance_issues",
     "score_efgm",
     "score_decision_efgm",
     "checklist_baseline",
     "grounding_calibration_baseline",
+    "independent_checklist_baseline",
     "weighted_linear_baseline",
 ]
