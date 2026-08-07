dbec3bf1dc7159009682420c40f70bd26155a6d7
Dan Auffrey <42249260+dauffrey@users.noreply.github.com>
2026-08-07 18:20:12 -0300

    Export v2 metric observation schema

--- src/efgm/__init__.py
+++ src/efgm/__init__.py
@@ -1,15 +1,17 @@
 from .schemas import EFGMInput, EFGMResult
-from .schemas_v2 import EFGMDecisionInput, EFGMDecisionResult
+from .schemas_v2 import EFGMDecisionInput, EFGMDecisionResult, MetricObservation
 from .scoring import score_efgm
-from .scoring_v2 import score_decision_efgm
+from .scoring_v2 import load_scoring_config, score_decision_efgm
 
 __version__ = "0.2.0"
 
 __all__ = [
     "EFGMInput",
     "EFGMResult",
+    "MetricObservation",
     "EFGMDecisionInput",
     "EFGMDecisionResult",
+    "load_scoring_config",
     "score_efgm",
     "score_decision_efgm",
 ]
