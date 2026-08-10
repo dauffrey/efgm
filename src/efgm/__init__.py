3762bbabb304178030efff09428fc13b6978ab36
Dan Auffrey <42249260+dauffrey@users.noreply.github.com>
2026-08-10 13:41:57 -0300

    Expose experimental v0.3 APIs

--- src/efgm/__init__.py
+++ src/efgm/__init__.py
@@ -6,6 +6,7 @@ from .baselines import (
 )
 from .schemas import EFGMInput, EFGMResult
 from .schemas_v2 import EFGMDecisionInput, EFGMDecisionResult, MetricObservation
+from .schemas_v3 import EFGMAgentGovernanceInput, EFGMAgentGovernanceResult
 from .scoring import score_efgm
 from .scoring_v2 import (
     IncompleteAssessmentError,
@@ -14,6 +15,17 @@ from .scoring_v2 import (
     load_scoring_config,
     research_provenance_issues,
     score_decision_efgm,
+)
+from .scoring_v3 import (
+    load_agent_governance_config,
+    score_agent_governance,
+)
+from .temporal_v0_3 import (
+    EFGMAgentState,
+    EFGMStateTransitionResult,
+    EFGMTemporalSequenceResult,
+    score_state_transition,
+    score_temporal_sequence,
 )
 
 __version__ = "0.2.0"
@@ -24,13 +36,22 @@ __all__ = [
     "MetricObservation",
     "EFGMDecisionInput",
     "EFGMDecisionResult",
+    "EFGMAgentGovernanceInput",
+    "EFGMAgentGovernanceResult",
+    "EFGMAgentState",
+    "EFGMStateTransitionResult",
+    "EFGMTemporalSequenceResult",
     "IncompleteAssessmentError",
     "ProvenanceError",
     "canonical_sha256",
     "load_scoring_config",
+    "load_agent_governance_config",
     "research_provenance_issues",
     "score_efgm",
     "score_decision_efgm",
+    "score_agent_governance",
+    "score_state_transition",
+    "score_temporal_sequence",
     "checklist_baseline",
     "grounding_calibration_baseline",
     "independent_checklist_baseline",
